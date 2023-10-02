2da821223ed5a7d5dbd182f81d0854753f316ea5
Samar Singh <singhsamar2002@gmail.com>
2023-10-02 22:59:29 +0530

    lil fix

--- lib/Views/CheckoutPage/checkoutPage1.dart
+++ lib/Views/CheckoutPage/checkoutPage1.dart
@@ -42,112 +42,109 @@ class _CheckoutPageState extends State<CheckoutPage> {
   Widget build(BuildContext context) {
     CheckoutViewModel().getCart(context);
 
-    return SingleChildScrollView(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          iconTheme: const IconThemeData(color: Colors.black),
-        ),
-        body: Padding(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 5,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 5,
+              ),
+              Align(
+                  alignment: Alignment.bottomLeft,
+                  child: giveText('Cart', 20, 600)),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: giveText('Add details to view cart', 14, 400)),
+              const SizedBox(
+                height: 35,
+              ),
+              TextFormField(
+                controller: tableCtrl,
+                onChanged: (value) {
+                  setLocal(key: 'tableNo', value: value);
+                  Constants.tableNo = int.parse(value);
+                },
+                //  readOnly: true,
+                //  initialValue: Constants.tableNo.toString(),
+                decoration: InputDecoration(
+                    label: const Text("Table Number"),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(color: Color(0xFF8C8C8C))),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    )),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: nameCtrl,
+                onChanged: (v) {
+                  Constants.name = v;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Enter Name',
+                  label: const Text("Customer Name"),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Color(0xFF8C8C8C))),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Color(0xFF8C8C8C))),
                 ),
-                Align(
-                    alignment: Alignment.bottomLeft,
-                    child: giveText('Cart', 20, 600)),
-                Align(
-                    alignment: Alignment.centerLeft,
-                    child: giveText('Add details to view cart', 14, 400)),
-                const SizedBox(
-                  height: 35,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: phnCtrl,
+                onChanged: (v) {
+                  Constants.phone = v;
+                },
+                readOnly: false,
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  prefix: giveText('+971 ', 14, 400),
+                  hintText: 'Enter Number',
+                  label: const Text("Customer Mobile"),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Color(0xFF8C8C8C))),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Color(0xFF8C8C8C))),
                 ),
-                TextFormField(
-                  controller: tableCtrl,
-                  onChanged: (value) {
-                    setLocal(key: 'tableNo', value: value);
-                    Constants.tableNo = int.parse(value);
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                  onPressed: () {
+                    context.go("/menu/${Constants.id}/checkout/checkout2");
                   },
-                  //  readOnly: true,
-                  //  initialValue: Constants.tableNo.toString(),
-                  decoration: InputDecoration(
-                      label: const Text("Table Number"),
-                      enabledBorder: OutlineInputBorder(
+                  child: Container(
+                      width: 396,
+                      height: 53,
+                      decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(12),
-                          borderSide:
-                              const BorderSide(color: Color(0xFF8C8C8C))),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      )),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  controller: nameCtrl,
-                  onChanged: (v) {
-                    Constants.name = v;
-                  },
-                  decoration: InputDecoration(
-                    hintText: 'Enter Name',
-                    label: const Text("Customer Name"),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: const BorderSide(color: Color(0xFF8C8C8C))),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: const BorderSide(color: Color(0xFF8C8C8C))),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  controller: phnCtrl,
-                  onChanged: (v) {
-                    Constants.phone = v;
-                  },
-                  readOnly: false,
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
-                    prefix: giveText('+971 ', 14, 400),
-                    hintText: 'Enter Number',
-                    label: const Text("Customer Mobile"),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: const BorderSide(color: Color(0xFF8C8C8C))),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: const BorderSide(color: Color(0xFF8C8C8C))),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextButton(
-                    onPressed: () {
-                      context.go("/menu/${Constants.id}/checkout/checkout2");
-                    },
-                    child: Container(
-                        width: 396,
-                        height: 53,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12),
-                            color: const Color(0xff88001f)),
-                        child: Center(
-                            child: Text(
-                          'Proceed to checkout',
-                          style: GoogleFonts.poppins(
-                              color: Colors.white,
-                              fontSize: 14,
-                              fontWeight: FontWeight.w600),
-                        ))))
-              ],
-            ),
+                          color: const Color(0xff88001f)),
+                      child: Center(
+                          child: Text(
+                        'Proceed to checkout',
+                        style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600),
+                      ))))
+            ],
           ),
         ),
       ),
