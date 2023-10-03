4489e4da3fcdff2636fd28bbec25fd43df141a4b
Samar Singh <singhsamar2002@gmail.com>
2023-10-03 12:01:09 +0530

    little fix

--- lib/Views/CheckoutPage/checkoutPage1.dart
+++ lib/Views/CheckoutPage/checkoutPage1.dart
@@ -50,7 +50,7 @@ class _CheckoutPageState extends State<CheckoutPage> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
               const SizedBox(
